3ce8881f90674545a087bc54d2fc44e29184e57f
MHGGreen <men61413@gmail.com>
2019-03-20 15:37:52 -0400

    Add files via upload

--- src/main/java/command/library/CmdDel.java
+++ src/main/java/command/library/CmdDel.java
@@ -17,7 +17,7 @@ public class CmdDel extends Command {
 	@Override
 	public void execute(IOutputter outputter) {
 		String fileName = this.getParameterAt(0);
-		ArrayList<FileSystemItem> files = new ArrayList<>(this.getDrive().getCurrentDirectory().getContent());
+		ArrayList<FileSystemItem> files = new ArrayList<FileSystemItem>(this.getDrive().getCurrentDirectory().getContent());
 		
 		for (Iterator<FileSystemItem> iterator = files.iterator(); iterator.hasNext();) {
 			FileSystemItem fileSystemItem = (FileSystemItem) iterator.next();
